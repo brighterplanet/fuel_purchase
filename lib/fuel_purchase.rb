212a1a9801e6c5bf04c49bd037a533fdc194fc57
Derek Kastner <dkastner@gmail.com>
2010-08-12 15:01:27 -0400

    Using emitter gem

--- lib/fuel_purchase.rb
+++ lib/fuel_purchase.rb
@@ -1,23 +1,10 @@
+require 'emitter'
+
 module BrighterPlanet
   module FuelPurchase
-    extend self
+    extend BrighterPlanet::Emitter
 
-    def included(base)
-      require 'cohort_scope'
-      require 'falls_back_on'
-      require 'falls_back_on/active_record_ext'
-
-      require 'fuel_purchase/carbon_model'
-      require 'fuel_purchase/characterization'
-      require 'fuel_purchase/data'
-      require 'fuel_purchase/summarization'
-
-      base.send :include, BrighterPlanet::FuelPurchase::CarbonModel
-      base.send :include, BrighterPlanet::FuelPurchase::Characterization
-      base.send :include, BrighterPlanet::FuelPurchase::Data
-      base.send :include, BrighterPlanet::FuelPurchase::Summarization
-    end
-    def fuel_purchase_model
+    def self.fuel_purchase_model
       if Object.const_defined? 'FuelPurchase'
         ::FuelPurchase
       elsif Object.const_defined? 'FuelPurchaseRecord'
