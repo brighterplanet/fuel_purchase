8a69eed24f31c34377e2ab1799356b402033c382
Seamus Abshere <seamus@abshere.net>
2011-06-08 23:58:50 -0500

    make tests green with new sniff

--- lib/fuel_purchase/data.rb
+++ lib/fuel_purchase/data.rb
@@ -2,26 +2,24 @@ module BrighterPlanet
   module FuelPurchase
     module Data
       def self.included(base)
+        base.create_table do
+          string 'fuel_type_id'
+          float  'volume'
+          string 'volume_units'
+          float  'cost'
+          string 'cost_units'
+          float  'price'
+          string 'price_units'
+          string 'zip_code_id'
+          string 'state_id'
+          string 'petroleum_administration_for_defense_district_id'
+          date   'date'
+        end
+
         base.data_miner do
-          schema do
-            string 'fuel_type_id'
-            float  'volume'
-            string 'volume_units'
-            float  'cost'
-            string 'cost_units'
-            float  'price'
-            string 'price_units'
-            string 'zip_code_id'
-            string 'state_id'
-            string 'petroleum_administration_for_defense_district_id'
-            date   'date'
-          end
-          
           process "Pull orphans" do
             FuelPrice.run_data_miner!
           end
-          
-          process :run_data_miner_on_belongs_to_associations
         end
       end
     end
