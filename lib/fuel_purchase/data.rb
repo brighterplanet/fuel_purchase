e272c683a0d5d6163981d63d9386c6aa963e43ba
Seamus Abshere <seamus@abshere.net>
2012-05-02 23:48:23 -0500

    remove unused units columns

--- lib/fuel_purchase/data.rb
+++ lib/fuel_purchase/data.rb
@@ -4,11 +4,8 @@ module BrighterPlanet
       def self.included(base)
         base.col :fuel_type_id
         base.col :volume, :type => :float
-        base.col :volume_units
         base.col :cost, :type => :float
-        base.col :cost_units
         base.col :price, :type => :float
-        base.col :price_units
         base.col :zip_code_id
         base.col :state_id
         base.col :petroleum_administration_for_defense_district_id
