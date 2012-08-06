f93d673fb8b906c1c46e0c44a728516b0c8d1caa
Derek Kastner <dkastner@gmail.com>
2012-08-06 13:53:56 -0500

    Enumerate emitter requires

--- lib/fuel_purchase.rb
+++ lib/fuel_purchase.rb
@@ -1,5 +1,11 @@
 require 'emitter'
 
+require 'fuel_purchase/impact_model'
+require 'fuel_purchase/characterization'
+require 'fuel_purchase/data'
+require 'fuel_purchase/relationships'
+require 'fuel_purchase/summarization'
+
 module BrighterPlanet
   module FuelPurchase
     extend BrighterPlanet::Emitter
